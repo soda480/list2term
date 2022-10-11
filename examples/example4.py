09e715cdc08de210ddd3529a30e11b65ed85da5f
Emilio Reyes <soda480@gmail.com>
2022-10-11 12:51:00 -0700

    feat: for pool_map context is optional
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- examples/example4.py
+++ examples/example4.py
@@ -29,7 +29,12 @@ def main(number):
     iterable = [(index, index + step) for index in range(0, number, step)]
     lookup = [':'.join(map(str, item)) for item in iterable]
     lines = Lines(lookup=lookup, use_color=True, show_index=True, show_x_axis=False)
-    results = pool_map(count_primes, iterable, lines)
+    # print to screen with lines context
+    results = pool_map(count_primes, iterable, context=lines)
+    # print to screen without lines context
+    # results = pool_map(count_primes, iterable)
+    # do not print to screen
+    # results = pool_map(count_primes, iterable, print_status=False)
     return sum(results.get())
 
 if __name__ == '__main__':
